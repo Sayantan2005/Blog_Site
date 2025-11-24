82cd7d18e6123edeed7aa072b25df24eeacecc88
Sayantan2005 <sayantans607@gmail.com>
2025-11-24 08:19:05 +0530

    create the themeProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,18 @@ import App from './App.jsx'
 import { Toaster } from 'sonner'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+import  ThemeProvider   from './components/ThemeProvider'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-       <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+
     </Provider>
-     <Toaster richColors position="top-right" />
-   
+    <Toaster richColors position="top-right" />
+
   </StrictMode>,
 )
